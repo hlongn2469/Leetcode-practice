57a8c275e7e10f3b698afc56ae06254584251ef6
hlongn2469 <78957509+hlongn2469@users.noreply.github.com>
2022-06-30 22:59:30 -0700

    Time: 3 ms (28.64%), Space: 57.7 MB (11.74%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
@@ -1,61 +1,25 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        // keeps track of current running sum
+        
+        int min = Integer.MAX_VALUE;
+        int j = 0;
         int sum = 0;
-        // handle sum dont reach target case
-        for(int i = 0; i < nums.length; i++){
-            sum+= nums[i];
+        for(int i = 0; i < nums.length;i++){
+            sum += nums[i];
+            while(sum >= target){
+                min = Math.min(min, i-j + 1);
+                j++;
+                sum-=nums[j-1];
+            }
         }
-        if(sum < target){
+        if(min == Integer.MAX_VALUE){
             return 0;
         }
-        
-        //reset sum
-        sum = 0;
-        
-        // left right pointers
-        int left = 0; 
-        int right = 0;
-        
-        // return value
-        int min_sub = Integer.MAX_VALUE;
-        
-        
-        
-        // loop through list
-        while(nums.length >= right){
-            
-            // if condition met, slide left pointer
-            if(sum >= target){
-                sum-=nums[left];
-                min_sub = Math.min(min_sub,right - left);
-                left++;
-            
-            // if condition not yet met, accumulate sum and slide right pointer
-            } else {
-                if(nums.length == right){
-                    break;
-                }
-                sum += nums[right++];
-            }
-            System.out.println("sum: " + sum);
-            System.out.println("min_sub: " + min_sub);
-        }
-        
-        // handle last index case
-        if(sum >= target){
-            min_sub = Math.min(min_sub,right-left);
-        }
-        
-        return min_sub;
+        return min;
     }
-    
     // [2,3,1,2,4,3]
-    //L   ^
-    //R       ^
-    // target = 7
-    // sum = 0
-    // min_subarray = 4;
-    
-    
+    //            i
+    //          j
+    // min = 2
+    // sum = 3
 }
